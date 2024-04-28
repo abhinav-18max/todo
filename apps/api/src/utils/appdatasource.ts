7f3afee6e1ca1dd8b1e356febc4a72afff8059a1
abhinav-18max <abhinav200218@gmail.com>
2024-04-28 20:42:58 +0530

    all finished

--- apps/api/src/utils/appdatasource.ts
+++ apps/api/src/utils/appdatasource.ts
@@ -1,13 +1,15 @@
 import { DataSource } from 'typeorm';
 import entities from './typeorm';
+import * as process from 'node:process';
+import { parse } from 'ts-jest';
 
 export const Appdatasource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  password: 'admin@123',
-  username: 'admin',
-  database: 'todo',
+  host: process.env.HOST,
+  port: parseInt(process.env.PORT),
+  password: process.env.PASSWORD,
+  username: process.env.USERNAME,
+  database: process.env.DATABASE,
   synchronize: true,
   logging: false,
   entities: entities,
